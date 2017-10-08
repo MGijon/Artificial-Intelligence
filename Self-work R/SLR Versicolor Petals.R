154e4a24584648c0b67813096c3a5a418173b0f5
Unknown <mgijon94@gmail.com>
2017-10-08 16:16:48 +0200

    Linear Models

--- Self-work R/SLR Versicolor Petals.R
+++ Self-work R/SLR Versicolor Petals.R
@@ -28,12 +28,23 @@ cor(Lenght_virginica, Width_virginica)     # 0.3221082
 plot(Lenght_versicolor, Width_versicolor, xlab = "Longitud pétalos", ylab = "Anchura pétalos", type = "p", col = "black")
 title("Versicolor", sub = "Relación entre los tamaños de los pétalos", col.main = "cadetblue", font.main = 4, cex.main = 3, col.sub = "grey") 
 
-regresion <- lm(Lenght_versicolor ~ Width_versicolor)
+regresion <- lm(Width_versicolor ~ Lenght_versicolor)
 summary(regresion)
 
-plot(Lenght_versicolor, Width_versicolor, xlab = "Longitud pétalos", ylab = "Anchura pétalos", type = "p", col = "black")
+plot(Lenght_versicolor, Width_versicolor, xlab = "Longitud pétalos", ylab = "Anchura pétalos", type = "p", col = "black", main = "Pétalos Versicolor")
 abline(regresion, col = "red")
 
+nuevas.longitudes = data.frame(Lenght_versicolor, seq = 3, 5)
 
+plot(Lenght_versicolor, Width_versicolor, xlab = "Longitud pétalos", ylab = "Anchura pétalos", type = "p", col = "black", main = "Pétalos Versicolor")
+abline(regresion, col = "red")
 
+ic <- predict(regresion, nuevas.longitudes, interval = "confidence")
+lines(nuevas.longitudes$Lenght_versicolor, ic[, 2], lty = 2, col = "blue")
+lines(nuevas.longitudes$Lenght_versicolor, ic[, 3], lty = 2, col = "blue")
 
+ic <- predict(regresion, nuevas.longitudes, interval = "predict")
+lines(nuevas.longitudes$Lenght_versicolor, ic[, 2], lty = 2, col = "green")
+lines(nuevas.longitudes$Lenght_versicolor, ic[, 3], lty = 2, col = "green")
+
+legend("topleft", legend = c("Recta regresión lineal", "Intervalo de confianza", "Intervalo de predicción"), col = c("red", "blue", "green"), lty = 1:2, cex = .8)
