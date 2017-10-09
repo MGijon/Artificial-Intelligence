d57717e362a1eb16521d5762b2dcffba8f0e36bc
Unknown <mgijon94@gmail.com>
2017-10-09 19:30:59 +0200

    Test AdalineSGD algorithm incomplete

---
+++ Examples R/Adaline/Test adalineSGD algorithm.R
@@ -0,0 +1,70 @@
+adalineSGD <- function(X, y, n_iter = 10, eta = 0.01) {
+
+  X[, dim(X)[2] + 1] <- 1
+  X <- as.matrix(X)
+  w <- as.matrix( rep(0, dim(X)[2]) )
+  
+  cost <- rep(0, n_iter)
+  errors <- rep(0, n_iter)
+  
+  for (i in 1:n_iter) {
+    
+    for ( j in sample(1:dim(X)[1], dim(X)[1], replace = FALSE) ) {
+      
+      z <- sum(w * X[j, ])
+      
+      if(z < 0.0) {
+        ypred <- -1
+      } else {
+        ypred <- 1
+      }
+      
+      if (ypred != y[j]) {
+        errors[i] = errors[i] + 1
+      }
+      
+      w <-  w + eta * (y[j] -z) * X[j, ]
+    }
+
+    cost[i] <- sum( (y - X %*% w) ^2) / 2
+  }
+  
+  infomatrix <- matrix( rep(0, 3 * n_iter), nrow = n_iter, ncol = 3)
+  infomatrix[, 1] <- 1:n_iter
+  infomatrix[, 2] <- log(cost)
+  infomatrix[, 3] <- error
+  
+  infodf <- as.data.frame(infomatrix)
+  names(infodf) <- c("epoch", "log(cost)", "error")
+  
+  return(infodf)
+}
+
+
+# Standardized vs non-standard data: Cost and error function
+
+n_eta = 0.0001
+n_iter = 100
+result1 <- adalineSGD(X, y, n_iter, n_eta)
+label <- rep("non-standard", dim(result1)[1])
+result2 <- adalineSGD(X_std, y, n_iter, eta)
+label <- rep("standard", dim(result2)[1])
+result2 <- cbind(label, result2)
+
+result2 <- cbind(label, result2)
+# To Be continued...
+# To Be continued...
+# To Be continued...
+# To Be continued...
+# To Be continued...
+# To Be continued...
+# To Be continued...
+# To Be continued...
+# To Be continued...
+# To Be continued...
+# To Be continued...
+# To Be continued...
+# To Be continued...
+
+
+# source: https://rpubs.com/FaiHas/199387
