142486d1e561683b7032e5890b83bd50d5d30950
Unknown <mgijon94@gmail.com>
2017-10-21 14:59:14 +0200

    Iris dataset and preprocessing

---
+++ Examples R/Adaline/Iris dataset and preprocessing.R
@@ -0,0 +1,51 @@
+# Load the iris dataset
+data (iris)
+str(iris)
+
+# sepal and petal dimensions
+X <- iris[1:100, 1:4]
+names(X) <- tolower(names(X))
+
+# initialize X_std dataframe
+X_std <- iris[1:100, 1:4]
+
+# define standarized data set
+for (i in 1:4){
+  X_std[, i] <- (X[, i] - mean(X[, i])) / sd(X[, i])
+}
+
+# visualize the first rows
+head(X, 4)
+head(X_std, 4)
+
+## ===============================
+
+# Binary classification vector acording to species: setosa (-1), versicolor(1)
+Y <- rep(1, 100)
+Y[which(iris[, 5] == "setosa")] <- -1
+Y[which(iris[, 5] == "versicolor")] <- 1
+
+# pre-process non-separable data points (ns stands for non-separable)
+# sepal and petal dimensions
+Xns <- iris[51:150, 1:4]
+names(Xns) <- tolower(names(Xns))
+
+# initialize X_std dataframe
+Xns_std <- iris[51:150, 1:4]
+
+# defined standariced dataframe
+for (i in 1:4){
+  Xns_std[, i] <- (Xns_std[, i] - mean(Xns_std[, i])) / sd(Xns_std[, i])
+}
+
+# Binary classification vector acording to species: setosa (-1), versicolor(1)
+l1 <- sum(iris[, 5] == 'versicolor')
+l2 <- sum(iris[, 5] == 'virginica')
+yns[1:l1] <- -1
+yns[(l1 + 1): (l1 +l2)] <- 1
+
+dim(Xns)
+length(yns)
+
+## ===============================
+
