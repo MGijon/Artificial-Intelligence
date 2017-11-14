71068d59fb0e03f07430609b82e9286d7dc8858b
Unknown <mgijon94@gmail.com>
2017-11-14 18:30:29 +0100

    Working on gensim
    making progress

--- Embeddings/gensim_models_1.py
+++ Embeddings/gensim_models_1.py
@@ -5,15 +5,14 @@ sentences = [['first', 'sentence'], ['second', 'sentence']]
 # train the model on the two sentences
 model = gm.Word2Vec(sentences, min_count = 1)  # default value for min_count is 5
 
-
-#print(model.wv['first'])                    # [  4.26240359e-03  -3.45680979e-03  ...  -1.74149941e-03]
-print(type(model.wv['first']))              # <class 'numpy.ndarray'>
-print(model.wv['first'].shape)              # (100,)
+print(type(model.wv['first']))              # <class 'numpy.ndarray'>, arrays shape = (100,)
 
 # Construct a list to save the words
-list_words = [[[word for word in sentence] for sentence in sentences]]
-print(list_words)
-print(type(list_words))                     # <class 'list'>
+list_words_embedded = []
+for sentence in sentences:
+    for word in sentence:
+        list_words_embedded.append(model.wv[word])
 
-list_words_embedded = matrix = [[[model.wv[word] for word in sentence] for sentence in sentences]]
-print(len(list_words_embedded))
+print(type(list_words_embedded))
+print(len(list_words_embedded))             # 4     -> here is the problem, the duplicity of words
+print(type(list_words_embedded[0]))         # <class 'numpy.ndarray'>
