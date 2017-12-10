05435c2826fb2fb5bdf38af71d4343e96e73faa4
Unknown <mgijon94@gmail.com>
2017-12-10 13:03:12 +0100

    textblob
    abandoned by now

--- Embeddings/Clean_text/textblob.py
+++ Embeddings/Clean_text/textblob.py
@@ -1,10 +1,31 @@
-from textblob import Word
+from textblob import TextBlob
 
-word = Word('plant')
-word.synsets[:5]
-
-
+# let's create our first textblob
+Poe_Raven = Textblob('Once upon a midnight dreary, while I pondered, weak and weary, Over many a quaint and curious volume of forgotten lore')
 
 
+## SENTIMENT ANALYSIS:
+## ==================
 
-# source : http://stevenloria.com/tutorial-wordnet-textblob/
+# The 'sentiment' property returns a namedtuple of the form 'Sentiment(polarity, subjetivity)'
+# , where polarity score is a float in range [-1.0, +1.0] and
+# the subjetivity score is a float in range [0, +1.0] where 0 is very objetive and 1.0 subjetive.
+
+print(Poe_Raven.sentiment)
+print(Poe_Raven.sentiment.polarity)
+print(Poe_Raven.sentiment.subjetivity)
+
+## TOKENIZATION:
+## ============
+
+print(Poe_Raven.words)
+
+print(Poe_Raven.sentences)
+
+
+
+
+
+
+
+# source : http://textblob.readthedocs.io/en/dev/quickstart.html#create-a-textblob
