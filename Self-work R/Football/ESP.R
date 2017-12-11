a3dd71e3b4d5e4055a89b28a481407f218160e2f
Unknown <mgijon94@gmail.com>
2017-12-11 12:52:03 +0100

    Football
    bucle a medio hacer

--- Self-work R/Football/ESP.R
+++ Self-work R/Football/ESP.R
@@ -1,7 +1,14 @@
 library(XML)
 
-url17ESP <- "http://www.livefutbol.com/calendario/esp-primera-division-2016-2017-spieltag_2/38/"
-url16ESP <- "http://www.livefutbol.com/calendario/esp-primera-division-2015-2016-spieltag/38/"
-liga17ESP <- readHTMLTable(url17ESP, header = TRUE)[[4]]
-liga16ESP <- readHTMLTable(url16ESP, header = TRUE)[[4]]
-liga17ESP
+url17 <- "http://www.livefutbol.com/calendario/esp-primera-division-2016-2017-spieltag_2/38/"
+url16 <- "http://www.livefutbol.com/calendario/esp-primera-division-2015-2016-spieltag/38/"  # from here there is a patron, let's automatize this
+url15 <- "http://www.livefutbol.com/calendario/esp-primera-division-2014-2015-spieltag/38/"
+liga17 <- readHTMLTable(url17, header = TRUE)[[4]]
+liga16 <- readHTMLTable(url16, header = TRUE)[[4]]
+liga15 <- readHTMLTable(url15, header = TRUE)[[4]]
+
+for (i in 1987:2015){
+  url <- paste0("http://www.livefutbol.com/calendario/esp-primera-division-", toString(i), "-", toString(i + 1), "-spieltag/38/")
+  
+}
+head(liga15)
