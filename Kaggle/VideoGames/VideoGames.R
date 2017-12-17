9583e7fb50c5a55c787abb586d41f5edcbed5d68
Unknown <mgijon94@gmail.com>
2017-12-17 20:45:58 +0100

    updated

---
+++ Kaggle/VideoGames/VideoGames.R
@@ -0,0 +1,31 @@
+library(ggplot2)
+library(dplyr)
+library(DT)
+library(tidyr)
+library(wesanderson)
+
+
+# Load the data.set
+videogames <- read.csv('vgsales.csv')
+summary(videogames)
+str(videogames)
+head(videogames)
+
+# Order the data by, for example, by Platform
+  # by default, sorting is ascending
+videogames <- videogames[order(videogames$Platform),]
+head(videogames)
+
+  # we can sort descending this way
+videogames <- videogames[order(-videogames$Global_Sales),]
+head(videogames)
+
+  # we can sort by two or more variables, for example
+videogames <- videogames[order(-videogames$EU_Sales, videogames$Global_Sales),]
+head(videogames)
+
+# =====================================================================
+
+Years <- videogames$Year
+
+plot(Years)
