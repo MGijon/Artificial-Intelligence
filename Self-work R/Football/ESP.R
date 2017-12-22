34df2246e59f20b9dbf373ed01204013cc7dd47a
Unknown <mgijon94@gmail.com>
2017-12-22 12:34:40 +0100

    updated
    ya tengo un data frame gordo

--- Self-work R/Football/ESP.R
+++ Self-work R/Football/ESP.R
@@ -7,17 +7,26 @@ liga17 <- readHTMLTable(url17, header = TRUE)[[4]]
 liga16 <- readHTMLTable(url16, header = TRUE)[[4]]
 liga15 <- readHTMLTable(url15, header = TRUE)[[4]]
 
+liga17$Year <- 2017
+head(liga17)
+liga16$Year <- 2016
+liga15$Year <- 2015
+head(liga16)
 head(liga15)
-liga15[1,] # ganador de la liga
+
+# cramos un data.frame 'gordote' con una nueva variable que indique el año:
+
+Ligas <- rbind.data.frame(liga17, liga16)
+Ligas <- rbind.data.frame(Ligas, liga15)
+
 
 for (i in 1987:2015){
   url <- paste0("http://www.livefutbol.com/calendario/esp-primera-division-", toString(i), "-", toString(i + 1), "-spieltag/38/")
-  print(url)
-  #datos[i - 1987] <- readHTMLTable(url, header = TRUE)[[4]]
+  datos<- readHTMLTable(url, header = TRUE)[[4]]
+  datos$Year <- i
+  Ligas <- rbind.data.frame(Ligas, datos)
 }
 
+summary(Ligas)
+str(Ligas)
 
-
-url88 <- "http://www.livefutbol.com/calendario/esp-primera-division-1987-1988-spieltag/38/"
-liga88 <- readHTMLTable(url88, header = TRUE)[[4]]
-liga88
