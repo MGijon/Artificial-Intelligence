08429c3808e7f8f88161c9a804a5fd0b1b2039ab
Unknown <mgijon94@gmail.com>
2017-12-22 12:14:13 +0100

    updated

--- Self-work R/Football/ESP.R
+++ Self-work R/Football/ESP.R
@@ -7,8 +7,12 @@ liga17 <- readHTMLTable(url17, header = TRUE)[[4]]
 liga16 <- readHTMLTable(url16, header = TRUE)[[4]]
 liga15 <- readHTMLTable(url15, header = TRUE)[[4]]
 
+head(liga15)
+liga15[1,] # ganador de la liga
+
 for (i in 1987:2015){
   url <- paste0("http://www.livefutbol.com/calendario/esp-primera-division-", toString(i), "-", toString(i + 1), "-spieltag/38/")
-  
+  datos[i - 1987] <- readHTMLTable(url, header = TRUE)[[4]]
 }
-head(liga15)
+
+
