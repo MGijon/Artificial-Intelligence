09ecce80bb41e4fa76c0c12f4a6d92425f7126f4
Unknown <mgijon94@gmail.com>
2017-12-23 16:54:02 +0100

    updated
    first picture

--- Embeddings/Clean_text/Hyper_Distances_graph_example_dog.py
+++ Embeddings/Clean_text/Hyper_Distances_graph_example_dog.py
@@ -21,14 +21,20 @@ Weights = [j[1] for j in Hyper_Distances_Dog]
 
 # Create a tuple (Synset origen, other one, weight)
     # Synset origen: take one suche us their distances is 0
-Origen = [s for s in Hyper_Distances_Dog if s[1] == 0]
+Origen = [s[0] for s in Hyper_Distances_Dog if s[1] == 0]
 #print(Origen)
-Edges = [(Origen, j[0], j[1]) for j in Hyper_Distances_Dog]
+Edges = [(Origen[0], j[0], j[1]) for j in Hyper_Distances_Dog]
 
 type(Edges[0])
+Edges[0]
 
 # adding the nodes
 G.add_nodes_from(Nodes)
 
 # adding the weighted Edges
 G.add_weighted_edges_from(Edges)
+
+# let's try to paint it
+nx.draw(G)
+plt.savefig('Hyper_Distances_graph_example_dog_1.png')
+plt.show()
