5b51d14bc631ea71893b9d2f47f9c08b4ef4221a
Unknown <mgijon94@gmail.com>
2018-02-08 18:33:54 +0100

    updated

--- Embeddings and manipulation of text/Gensim/Automatic.py
+++ Embeddings and manipulation of text/Gensim/Automatic.py
@@ -4,12 +4,17 @@ import gensim.models as gm
 filepath = 'Data/words_source.csv'
 data = pd.read_csv(filepath, sep = ';')
 
+data.head()
+
 route = '/Users/manuelgijonagudo/Documents/Programación/GIT/Data/GoogleNews-vectors-negative300.bin.gz'
 model = gm.KeyedVectors.load_word2vec_format(route, binary = True)
 
-resultado = model.most_similar( positive = ['woman', 'king'], negative = ['man'], topn = 1)
 
-resultado
-type(resultado)
+resultado = model.most_similar( positive = [data['Pos1'][0], data['Pos2'][0]],
+                                negative = [data['Neg'][0]], topn = 1)
 
-resultado[0][0]
+data['Resoult'][0] = resultado
+data['Word'][0] = resultado[0][0]
+data['Distance'][0] = resultado[0][1]
+
+data.head()
