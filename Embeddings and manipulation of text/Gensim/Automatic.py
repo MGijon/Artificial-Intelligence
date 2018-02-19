351eeff6d5cebff12e4090c40e37f0ab4f4b852a
Unknown <mgijon94@gmail.com>
2018-02-19 20:37:47 +0100

    updated

--- Embeddings and manipulation of text/Gensim/Automatic.py
+++ Embeddings and manipulation of text/Gensim/Automatic.py
@@ -3,6 +3,7 @@ import gensim.models as gm
 
 filepath = 'Data/words_source.csv'
 data = pd.read_csv(filepath, sep = ';')
+data.head()
 
 route = '/Users/manuelgijonagudo/Documents/Programación/GIT/Data/GoogleNews-vectors-negative300.bin.gz'
 model = gm.KeyedVectors.load_word2vec_format(route, binary = True)
