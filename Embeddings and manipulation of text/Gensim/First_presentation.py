6c09ee805678fb6278222a0f2552602b4fe39277
Unknown <mgijon94@gmail.com>
2018-03-03 19:11:56 +0100

    updated

--- Embeddings and manipulation of text/Gensim/First_presentation.py
+++ Embeddings and manipulation of text/Gensim/First_presentation.py
@@ -1,5 +1,7 @@
-import pandas as pd
 import gensim.models as gm
+import pandas as pd
+from sklearn.decomposition import PCA
+from matplotlib import pyplot as plt
 
 # ruta mac:
 filepath = 'Data/words_source_v2.csv'
@@ -8,13 +10,39 @@ filepath = 'Data/words_source_v2.csv'
 data = pd.read_csv(filepath, sep = ';')
 data.head()
 
-'''
 # ruta mac:
 route = '/Users/manuelgijonagudo/Documents/Programación/GIT/Data/GoogleNews-vectors-negative300.bin.gz'
 # ruta windows:
 # route = 'D:\GIT\Data\GoogleNews-vectors-negative300.bin.gz'
 model = gm.KeyedVectors.load_word2vec_format(route, binary = True)
 
+data = data[['Pos1', 'R1', 'R2', 'R3', 'R4', 'R5']]
 
-i = 0
-'''
+
+
+
+
+words_list = ['man', 'women', 'king', 'queen']
+
+def construct_word_dict(list):
+    dict = {}
+    for i in list:
+        dict[i] = model.wv.vocab[i]
+    return dict
+
+words_dict = construct_word_dict(words_list)
+
+
+X = model[words_dict]
+pca = PCA(n_components = 2)
+result = pca.fit_transform(X)
+
+
+plt.scatter(result[:, 0][:2], result[:, 1][:2], c = 'green', label = 'input')
+plt.scatter(result[:, 0][2:], result[:, 1][2:], c = 'red', label = 'output')
+words = list(words_dict)
+for i, word in enumerate(words):
+	print(i, word)
+	plt.annotate(word, xy = (result[i, 0], result[i, 1]))
+plt.legend(loc = 2)
+plt.show()
