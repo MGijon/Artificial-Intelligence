917dadc0a7fdce6051eef46eb7909084818a55f0
Unknown <mgijon94@gmail.com>
2018-03-09 11:14:44 +0100

    updated

--- Kaggle/VideoGames R/VideoGames.R
+++ Kaggle/VideoGames R/VideoGames.R
@@ -6,12 +6,13 @@ library(wesanderson)
 
 
 # Load the data.set
-videogames <- read.csv('Data/vgsales.csv', stringsAsFactors = FALSE)
+videogames <- read.csv('/Users/manuelgijonagudo/Documents/Programación/GIT/Machine-Learning/Kaggle/VideoGames R/Data/vgsales.csv', stringsAsFactors = FALSE)
 
 summary(videogames)
-str(videogames)
-head(videogames)
+#str(videogames)
+#head(videogames)
 
+videogames$Year = as.numeric(videogames$Year)
 
 # plot the sales against the year
 plot(videogames$Year, videogames$Global_Sales, main = 'Year - Global_Sales')
