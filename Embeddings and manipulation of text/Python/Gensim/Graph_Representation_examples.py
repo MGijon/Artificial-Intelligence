12866430d13704f0aefa465b00aca1fea2f801f9
Unknown <mgijon94@gmail.com>
2018-03-13 20:15:30 +0100

    update
    not finished yet

---
+++ Embeddings and manipulation of text/Python/Gensim/Graph_Representation_examples.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import gensim.models as gm
+import networkx as nx
+import matplotlib.pyplot as plt
+
+route = '/Users/manuelgijonagudo/Documents/Programación/GIT/Data/GoogleNews-vectors-negative300.bin.gz'
+model = gm.KeyedVectors.load_word2vec_format(route, binary = True)
+
+node_1 = 'man'
+node_2 = 'woman'
+node_3 = 'king'
+node_4 = model.most_similar( positive = [node_1, node_3],
+                                negative = [node_2], topn = 1)[0]
+
+nodes = [node_1, node_2, node_3, node_4]
+
+dis_1_2 = model.similarity(node_1, node_2)
+dis_1_3 = model.similarity(node_1, node_3)
+dis_1_4 = model.similarity(node_1, node_4)
+dis_2_3 = model.similarity(node_2, node_3)
+dis_2_4 = model.similarity(node_2, node_4)
+dis_3_4 = model.similarity(node_3, node_4)
