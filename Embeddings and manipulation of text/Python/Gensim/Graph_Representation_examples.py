27bf757bfa5086cf0c47b31fce03aa08d4fbe76e
Unknown <mgijon94@gmail.com>
2018-03-13 21:11:51 +0100

    updated

--- Embeddings and manipulation of text/Python/Gensim/Graph_Representation_examples.py
+++ Embeddings and manipulation of text/Python/Gensim/Graph_Representation_examples.py
@@ -9,8 +9,8 @@ model = gm.KeyedVectors.load_word2vec_format(route, binary = True)
 node_1 = 'man'
 node_2 = 'woman'
 node_3 = 'king'
-node_4 = model.most_similar( positive = [node_1, node_3],
-                                negative = [node_2], topn = 1)[0]
+result = model.most_similar( positive = [node_2, node_3], negative = [node_1], topn = 1)
+node_4 = result[0][0]
 
 nodes = [node_1, node_2, node_3, node_4]
 
@@ -20,3 +20,5 @@ dis_1_4 = model.similarity(node_1, node_4)
 dis_2_3 = model.similarity(node_2, node_3)
 dis_2_4 = model.similarity(node_2, node_4)
 dis_3_4 = model.similarity(node_3, node_4)
+
+edges = [dis_1_2, dis_1_3, dis_1_4, dis_2_3, dis_2_4, dis_3_4]
