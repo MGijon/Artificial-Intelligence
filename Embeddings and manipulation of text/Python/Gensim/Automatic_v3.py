389cfedbf267bc152b14ad6f0da418e1e54bdab1
Unknown <mgijon94@gmail.com>
2018-03-15 23:47:40 +0100

    updated

--- Embeddings and manipulation of text/Python/Gensim/Automatic_v3.py
+++ Embeddings and manipulation of text/Python/Gensim/Automatic_v3.py
@@ -7,18 +7,18 @@ import gensim.models as gm
 #logger.setLevel(logging.INFO)
 
 # ruta mac:
-# filepath = 'Data/Ofensivas.csv'
+filepath = 'Data/Ofensivas.csv'
 # ruta windows:
-filepath = ''
+#filepath = ''
 data = pd.read_csv(filepath, sep = ';')
 #logger.info('File imported')
 data.head()
 
 # model
 # ruta mac:
-# route = '/Users/manuelgijonagudo/Documents/Programación/GIT/Data/GoogleNews-vectors-negative300.bin.gz'
+route = '/Users/manuelgijonagudo/Documents/Programación/GIT/Data/GoogleNews-vectors-negative300.bin.gz'
 # ruta windows:
-route = 'D:\GIT\Data\GoogleNews-vectors-negative300.bin.gz'
+# route = 'D:\GIT\Data\GoogleNews-vectors-negative300.bin.gz'
 model = gm.KeyedVectors.load_word2vec_format(route, binary = True)
 #logging.info('Model upload, starting with the process')
 
@@ -29,6 +29,7 @@ while isinstance(data['Pos1'][i] , str):
     if isinstance(data['R1'][i] , str) == False:
         # añado un chivato para ver dónde está la palabra fuera del diccionario
         #print(data['Pos1'])
+        '''
         resultado = model.most_similar( positive = [data['Pos1'][i], data['Pos2'][i]],
                                         negative = [data['Neg'][i]], topn = 10)
         data['R1'][i] = resultado[0]
@@ -41,7 +42,22 @@ while isinstance(data['Pos1'][i] , str):
         data['R8'][i] = resultado[7]
         data['R9'][i] = resultado[8]
         data['R10'][i] = resultado[9]
-
+        '''
+        try:
+            resultado = model.most_similar( positive = [data['Pos1'][i], data['Pos2'][i]],
+                                            negative = [data['Neg'][i]], topn = 10)
+            data['R1'][i] = resultado[0]
+            data['R2'][i] = resultado[1]
+            data['R3'][i] = resultado[2]
+            data['R4'][i] = resultado[3]
+            data['R5'][i] = resultado[4]
+            data['R6'][i] = resultado[5]
+            data['R7'][i] = resultado[6]
+            data['R8'][i] = resultado[7]
+            data['R9'][i] = resultado[8]
+            data['R10'][i] = resultado[9]
+        except KeyError:
+            i = i + 1
     i = i + 1
 
 #logging.info('Process finished')
