d962653be30d2bfd439d7b22d1a896e8ce2f2567
Unknown <mgijon94@gmail.com>
2018-03-20 20:29:47 +0100

    updated

---
+++ Self-work Python/Clasification -Supervised Learning/Linear regression/Boston_house_prices.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
+bh = datasets.load_boston()
+
+
