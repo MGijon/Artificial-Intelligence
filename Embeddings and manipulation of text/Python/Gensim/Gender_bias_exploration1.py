654a3e1d504ad9953d3eb20871d545ed5fac1a7c
Unknown <mgijon94@gmail.com>
2018-03-21 09:24:22 +0100

    updated

--- Embeddings and manipulation of text/Python/Gensim/Gender_bias_exploration1.py
+++ Embeddings and manipulation of text/Python/Gensim/Gender_bias_exploration1.py
@@ -3,6 +3,10 @@ import pandas as pd
 import gensim.models as gm
 import matplotlib.pyplot as plt
 import logging
+from nltk.corpus import wordnet as wn
+
+## Logging
+## =======
 
 # create logger
 logger = logging.getLogger('report_Gender_bias_debiation')
@@ -16,15 +20,39 @@ fh.setFormatter(formatter)
 # add the handlers to the logger
 logger.addHandler(fh)
 
+## Load the pre-trained model
+## ==========================
+
 # ruta mac:
 route = '/Users/manuelgijonagudo/Documents/Programación/GIT/Data/GoogleNews-vectors-negative300.bin.gz'
 # ruta windows:
 # route = 'D:\GIT\Data\GoogleNews-vectors-negative300.bin.gz'
 model = gm.KeyedVectors.load_word2vec_format(route, binary = True)
-logger.info('Model loaded successfully')
+logger.info('Model loaded successfully\n')
+
+# TENGO QUE ENCONTRAR UNA MANERA MEJOR DE DEFINIR LOS CONJUNTOS, SUPONGO QUE
+# UTILIZANDO : synsets de ser vivo y artefacto: living_thing
+# para cada hipónimo tendremos un cierto porcentaje de elementos que
+# cuya imagen por la regularidad tiene una o más palabras a esa distancia
+# por ahora eso, en el futuro ver cuántas palabras haya esa distancia
+
+## Builting synsets
+## ================
+
 
 total_size = len(model.wv.vocab)
+
+## Threshold and loops
+## ===================
 threshold = 0.25
-logger.info('Total size of vocabulary: %s\nThreshold fixed at %s\n', total_size, threshold)
+logger.info('Threshold fixed at %s\n', threshold)
+
+
+## Statistics
+## ==========
+
+
+## Graphic representation
+## ======================
 
 #print(model.most_similar( positive = ['woman', 'doctor'], negative = ['man'], topn = 1))
