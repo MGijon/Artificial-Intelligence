33f7977338c2a0ef4b8395f8efec5a8a0f77a3bb
Unknown <mgijon94@gmail.com>
2018-03-23 11:34:06 +0100

    updated

--- Kaggle/Pokemon R/PokemonR.R
+++ Kaggle/Pokemon R/PokemonR.R
@@ -25,5 +25,24 @@ head(train)
   # combats
 summary(combats)  
 head(combats)
+  
+  # list of diffent types of pokemon
+Type <- levels(factor(pokemon$Type.1))
+Type
 
+  # means of different quantities by first type 
+tapply(pokemon$HP, pokemon$Type.1, mean)
+tapply(pokemon$Attack, pokemon$Type.1, mean)
+tapply(pokemon$Sp..Atk, pokemon$Type.1, mean)
+tapply(pokemon$Defense, pokemon$Type.1, mean)
+tapply(pokemon$Sp..Def, pokemon$Type.1, mean)
+tapply(pokemon$Speed, pokemon$Type.1, mean)
 
+ # means of difernet quantities by seconf type
+    # obs: there are pokemons with no second type
+tapply(pokemon$HP, pokemon$Type.2, mean)
+tapply(pokemon$Attack, pokemon$Type.2, mean)
+tapply(pokemon$Sp..Atk, pokemon$Type.2, mean)
+tapply(pokemon$Defense, pokemon$Type.2, mean)
+tapply(pokemon$Sp..Def, pokemon$Type.2, mean)
+tapply(pokemon$Speed, pokemon$Type.2, mean)
