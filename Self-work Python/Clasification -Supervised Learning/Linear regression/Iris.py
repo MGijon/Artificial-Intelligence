a94bded8b2ddfeb3c5968de00d0acb1eabc9e1ae
Unknown <mgijon94@gmail.com>
2018-03-26 15:27:11 +0200

    updated

---
+++ Self-work Python/Clasification -Supervised Learning/Linear regression/Iris.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
+iris = datasets.load_iris()
+
