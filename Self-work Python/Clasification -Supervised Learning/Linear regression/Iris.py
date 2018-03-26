863b21f60fb8d2c57e5eaffd27bc441a92cb798e
Unknown <mgijon94@gmail.com>
2018-03-26 23:16:51 +0200

    updated

--- Self-work Python/Clasification -Supervised Learning/Linear regression/Iris.py
+++ Self-work Python/Clasification -Supervised Learning/Linear regression/Iris.py
@@ -1,7 +1,13 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
 iris = datasets.load_iris()
 
+X = iris.data
+Y = iris.target
+
+flowers = iris.target_names
+features = iris.feature_names
