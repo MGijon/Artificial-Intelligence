a9b744a525b114f19cfb51c6a60fabbeb065b046
Unknown <mgijon94@gmail.com>
2018-03-28 12:52:43 +0200

    updated

--- Self-work Python/Clasification -Supervised Learning/Linear regression/Boston_house_prices.py
+++ Self-work Python/Clasification -Supervised Learning/Linear regression/Boston_house_prices.py
@@ -5,4 +5,20 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 bh = datasets.load_boston()
 
+features = bh.feature_names
+X = bh.data
+Y = bh.target
 
+#X_train = Xsjdoa
+#X_test = zd
+'''
+X = diabetes.data[:, np.newaxis, 2]
+X_train = X[:-20]
+X_test = X[-20:]
+y_train = diabetes.target[:-20]
+y_test = diabetes.target[-20:]
+'''
+
+regression = linear_model.LinearRegression()
+regresion.fix(X_train, Y_train)
+y_pred = regression.predict(X_test)
