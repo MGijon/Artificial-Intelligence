918a8fc45e6e0d4a138c71cadabaf2b48f20992d
Unknown <mgijon94@gmail.com>
2018-03-28 11:20:26 +0200

    updated

--- Self-work Python/Clasification -Supervised Learning/Linear regression/Iris_by_flower.py
+++ Self-work Python/Clasification -Supervised Learning/Linear regression/Iris_by_flower.py
@@ -1,3 +1,7 @@
+'''
+    Regression models for each caracteristic (sepal and petal size) or each flower, not a
+    classification problem.
+'''
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -12,8 +16,12 @@ Y = iris.target
 
 flowers = iris.target_names
 features = iris.feature_names
-flowers
-print(X[:, :2]) # two first features: sepal length (cm) and sepal width (cm)
+features
+# print(X[:, :2]) # two first features: sepal length (cm) and sepal width (cm)
+
+## ===== ##
+## SEPAL ##
+## ===== ##
 
 Sepal = X[:, :2]
 Sepal_length = Sepal[:, 0]
@@ -24,11 +32,11 @@ plt.scatter(Sepal_length[:50], Sepal_width[:50], label = 'Setosa')
 plt.scatter(Sepal_length[50:100], Sepal_width[50:100], label = 'Versicolor')
 plt.scatter(Sepal_length[100:], Sepal_width[100:], label = 'Virginica')
 plt.legend()
-plt.xlabel('Sepal length')
-plt.ylabel('Sepal Width')
+plt.xlabel('Sepal length (cm)')
+plt.ylabel('Sepal Width (cm)')
 plt.show()
 
-# we select just the Setosa and Versicolor flowers by now
+# just selecting Setosa and Versicolor species
 
 SL_Setosa = Sepal_length[:50]
 SL_Versicolor = Sepal_length[50:100]
@@ -39,67 +47,21 @@ plt.figure(figsize = (10, 7.5))
 plt.scatter(SL_Setosa, SW_Setosa, label = 'Setosa')
 plt.scatter(SL_Versicolor, SW_Versicolor, label = 'Versicolor')
 plt.legend()
-plt.xlabel('Sepal length')
-plt.ylabel('Sepal Width')
+plt.xlabel('Sepal length (cm)')
+plt.ylabel('Sepal Width (cm)')
 plt.show()
 
-################################################################################
-'''
-# now we define the train and the test sets
+## LINEAR REGRESSION
+## =================
 
-X_train = Sepal[:75] # 75% of the samples
-X_test = Sepal[75:100]
-
-Y_train = iris.target[:75]
-Y_test = iris.target[75:100]
-
-# regression object
-regression = linear_model.LinearRegression()
-# training
-regression.fit(X_train, Y_train)
-# prediction
-y_pred = regression.predict(X_test)
-
-# plotting
-print('Coeficients: \n', regression.coef_)
-
-print("Mean squared error: \n %.2f" % mean_squared_error(Y_test, y_pred))
-
-print('Variance Score: \n %.2f' %r2_score(Y_test, y_pred))
-'''
-
-X = iris.data[:, np.newaxis, 2]
-X = X[:50]
-X.shape
-X_train = X[:-20]
-X_test = X[-20:]
-Y = iris.target[:50]
-y_train = Y[:-20]
-y_test = Y[-20:]
-# creo que el problema es que he de limitar antes los datos con los que trabajo
-# regression object
-regression = linear_model.LinearRegression()
-len(X)
-# training
-regression.fit(X_train, y_train)
-
-# prediction
-y_pred = regression.predict(X_test)
-
-# plotting
-print('Coeficients: \n', regression.coef_)                                  # [ 938.23786125]
-
-print("Mean squared error: \n %.2f" % mean_squared_error(y_test, y_pred))   # 2548.07
-
-print('Variance Score: \n %.2f' %r2_score(y_test, y_pred))                  # 0.47
-
-
-#plt.scatter(X_test, y_test, label = 'Data values')
-plt.scatter(SL_Setosa, SW_Setosa, label = 'Setosa')
-plt.plot(X_test, y_pred, color = 'r', label = 'Predict values')
-plt.title('')
-plt.legend()
-plt.show()
+# Petal length and width
+X = iris.data[:, :2]
+X = X[:100]
+X_train = X[:40]
+X_train
+len(X_train)
+np.insert(X_train, X[50:90])
+len(X_train)
 
 
 ################################################################################
