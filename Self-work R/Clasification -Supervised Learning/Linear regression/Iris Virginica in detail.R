689aebf1f6110ce0fda3fe9ca351fcbe31162ca5
Unknown <mgijon94@gmail.com>
2018-04-01 18:35:41 +0200

    updated

--- Self-work R/Clasification -Supervised Learning/Linear regression/Iris Virginica in detail.R
+++ Self-work R/Clasification -Supervised Learning/Linear regression/Iris Virginica in detail.R
@@ -61,3 +61,48 @@ qqline(residuos)
 ###########
 ## SEPAL ##
 ###########
+
+
+cor(Virginica$sepal.length, Virginica$sepal.width) # 0.4572278
+
+# linear regression
+regresion <- lm(sepal.width ~ sepal.length, data = Virginica)
+summary(regresion)
+
+# basic plot
+plot(x = Virginica$sepal.length, y = Virginica$sepal.width, xlab = "Sepal length", ylab = "Sepal width", main = "Virginica sepal linear regression")
+abline(regresion)
+
+# predictions calculations
+new.flowers <- data.frame(sepal.length = seq(from = 5, to = 8, by = .1))
+predict(regresion, new.flowers)
+
+# inference from the model
+confint(regresion)
+confint(regresion, level = .3)
+
+# graphic complete
+plot(x = Virginica$sepal.length, y = Virginica$sepal.width, xlab = "Sepal length", ylab = "Sepal width", main = "Virginica sepal linear regression")
+abline(regresion)
+
+  # confidence intervals
+ic <- predict(regresion, new.flowers, interval = "confidence")
+lines(new.flowers$sepal.length, ic[, 2], lty = 2, col = "blue")
+lines(new.flowers$sepal.length, ic[, 3], lty = 2, col = "blue")
+
+  # prediction intervals
+ic <- predict(regresion, new.flowers, interval = "prediction")
+lines(new.flowers$sepal.length, ic[, 2], lty = 2, col = "red")
+lines(new.flowers$sepal.length, ic[, 3], lty = 2, col = "red")
+
+
+## Diagnosis of the model
+## ======================
+
+residuos <- rstandard(regresion)
+valores.ajustados <- fitted(regresion)
+plot(x = valores.ajustados, y = residuos, xlab = "Fitted values", ylab = "Residuals", main = "Standarized resuduals against fitted values")
+
+# normality hypotesis is check by a QQ plot of the residual
+qqnorm(residuos)
+qqline(residuos)
