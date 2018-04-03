bfb7014d11f20a7791f4900c0741d5cfa0641da7
Unknown <mgijon94@gmail.com>
2018-04-03 17:41:07 +0200

    updated

--- Self-work Python/Clasification -Supervised Learning/Decision tree/Iris.py
+++ Self-work Python/Clasification -Supervised Learning/Decision tree/Iris.py
@@ -1,15 +1,36 @@
 from sklearn.datasets import load_iris
 from sklearn import tree
+import numpy as np
+import random
 
 iris = load_iris()
+
+## splitter = 'best' (by default)
+## ================
 
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(iris.data, iris.target)
 
-clf.predict(iris.data[:1, :])
-clf.predict_proba(iris.data[:1, :])
+clf.predict(iris.data[:50, :])
+clf.predict_proba(iris.data[:50, :])
 
 clf.predict(iris.data[50:100, :])
 clf.predict_proba(iris.data[50:100, :])
+
 clf.predict(iris.data[100:, :])
 clf.predict_proba(iris.data[100:, :])
+
+## splitter = 'random'
+## ==================
+
+clf_2 = tree.DecisionTreeClassifier(splitter = 'random')
+clf_2 = clf_2.fit(iris.data, iris.target)
+
+clf_2.predict(iris.data[:50, :])
+clf_2.predict_proba(iris.data[:50, :])
+
+clf_2.predict(iris.data[50:100, :])
+clf_2.predict_proba(iris.data[50:100, :])
+
+clf_2.predict(iris.data[100:, :])
+clf_2.predict_proba(iris.data[100:, :])
