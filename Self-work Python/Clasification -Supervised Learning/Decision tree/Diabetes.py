ee37b87d7e42406442845ff7385bb1d9cea5a666
Unknown <mgijon94@gmail.com>
2018-04-05 12:26:17 +0200

    updated

--- Self-work Python/Clasification -Supervised Learning/Decision tree/Diabetes.py
+++ Self-work Python/Clasification -Supervised Learning/Decision tree/Diabetes.py
@@ -10,3 +10,19 @@ clf.predict(diabetes.data[:1, :])
 clf.predict(diabetes.data[:3, :])
 
 clf.predict(diabetes.data[:5, :])
+
+# Now I'll train the model with less data
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(diabetes.data[:50, :], diabetes.target[:50])
+
+predictions = clf.predict(diabetes.data[50:, :])
+
+wrongs = 0
+j = 0
+for i in predictions:
+    if diabetes.target[j] != i:
+        wrongs += 1
+    j += 1
+wrongs
+
+# 388 bad predictions!!
