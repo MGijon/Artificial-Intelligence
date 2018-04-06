29f0fc136376037002ac434fa02d96f9deed7a46
Unknown <mgijon94@gmail.com>
2018-04-06 11:32:28 +0200

    updated

---
+++ Self-work Python/Clasification -Supervised Learning/Decision tree/IrisPandas.py
@@ -0,0 +1,12 @@
+import sklearn.datasets as datasets
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+iris = datasets.load_iris()
+df = pd.DataFrame(iris.data, columns = iris.feature_names)
+y = iris.target
+
+dtc = DecisionTreeClassifier()
+dtc = dtc.fit(df, y)
+
+dtc.predict()
