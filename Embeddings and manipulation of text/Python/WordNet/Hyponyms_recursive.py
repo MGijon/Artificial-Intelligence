8e7ab51a4178092b11559f299776ea28223bed67
Unknown <mgijon94@gmail.com>
2018-04-08 20:15:05 +0200

    updated
    no funciona ni a tiros

--- Embeddings and manipulation of text/Python/WordNet/Hyponyms_recursive.py
+++ Embeddings and manipulation of text/Python/WordNet/Hyponyms_recursive.py
@@ -1,7 +1,6 @@
 from nltk.corpus import wordnet as wn
 
-conjunto = []
-syn = wn.synsets('dog')[0]
+SYN = wn.synsets('dog')[0]
 
 lista = syn.hyponyms()
 
@@ -19,3 +18,45 @@ def numero_ramas(syn):
     Calculamos el número de hijos de cada nodo del árbol
     '''
     return len(syn.hyponyms())
+
+def lista_hijos(syn):
+    '''
+    Devuelve una lista con el número de hijos de cada nodo de sus hijos
+    '''
+    return [numero_ramas(i) for i in syn.hyponyms()]
+
+def Cest_fini(lista):
+    '''
+    Si no hay hijos nos devuelve un True
+    '''
+    if len(lista) == 0:
+        return True
+    else:
+        lista = []
+        return False
+
+def bucle_idiota(syn):
+    '''
+    Función recursiva que devuelve una lista con los hypónimos al completo
+    '''
+    conjunto = []
+    conjunto.append(syn)
+    last_branches = [x for x in syn.hyponyms()]
+    while last_branches != []:
+        for i in last_branches:
+            conjunto.append(i)
+            last_branches.remove(i)
+            last_branches.append(i.hyponyms())
+
+    return conjunto
+
+bucle_idiota(SYN)
+'''
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+'''
