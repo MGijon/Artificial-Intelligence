0c486bbd0270ec33fdc371f6dbf5a57f951db14e
Unknown <mgijon94@gmail.com>
2018-04-16 16:46:58 +0200

    updated

--- Embeddings and manipulation of text/Python/Gensim/Gender_bias_exploration2.py
+++ Embeddings and manipulation of text/Python/Gensim/Gender_bias_exploration2.py
@@ -8,6 +8,39 @@ from nltk.corpus import wordnet as wn
 route = '/Users/manuelgijonagudo/Documents/Programación/GIT/Data/GoogleNews-vectors-negative300.bin.gz'
 model = gm.KeyedVectors.load_word2vec_format(route, binary = True)
 
+SYN = wn.synsets('dog')[0]
+lista = SYN.hyponyms()
+def numero_ramas(syn):
+    '''
+    Calculamos el número de hijos de cada nodo del árbol
+    '''
+    return len(syn.hyponyms())
+
+conjunto = []
+def recursiva(C_syn):
+    '''
+    Función recursiva que devuelve una lista con los hypónimos al completo.
+    INPUT: conjunto de synsets (1 elemento)
+    OUTPUT: lista de todos los synsets hipónimos
+    '''
+    for j in C_syn:
+        if numero_ramas(j) == 0:
+            conjunto.append(j)
+        else:
+            conjunto.append(j)
+            for i in j.hyponyms():
+                recursiva([i])
+    return conjunto
+
+recursiva([SYN])
+len(recursiva([SYN]))
+
+Artifact = recursiva([wn.synsets('artifact')[0]])
+
+Artifact[0].lemma_names()
+
+
+
 result = model.most_similar(positive = ['women', 'king'], negative = ['men'], topn = 1)
 
 result[0][0]
