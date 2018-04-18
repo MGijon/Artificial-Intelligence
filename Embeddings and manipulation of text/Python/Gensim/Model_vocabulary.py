6087af1a43ec2a49fb5f2a23e1e948560d04b251
Unknown <mgijon94@gmail.com>
2018-04-18 17:42:57 +0200

    updated

--- Embeddings and manipulation of text/Python/Gensim/Model_vocabulary.py
+++ Embeddings and manipulation of text/Python/Gensim/Model_vocabulary.py
@@ -11,4 +11,24 @@ model = gm.KeyedVectors.load_word2vec_format(route, binary = True)
 data = pd.DataFrame()
 data['WORDS'] = list(model.vocab.keys())
 
-data.to_csv('Data/Model_Vocabulary', sep = ';', index = False)
+data.to_csv('Data/Model_Vocabulary.csv', sep = ';', index = False)
+
+data1 = pd.DataFrame()
+data1['WORDS'] = list(model.vocab.keys())[:500000]
+data1.to_csv('Data/Model_Vocabulary_part1.csv', sep = ';', index = False)
+
+data2 = pd.DataFrame()
+data2['WORDS'] = list(model.vocab.keys())[500000:1000000]
+data2.to_csv('Data/Model_Vocabulary_part2.csv', sep = ';', index = False)
+
+data3 = pd.DataFrame()
+data3['WORDS'] = list(model.vocab.keys())[1000000:1500000]
+data3.to_csv('Data/Model_Vocabulary_part3.csv', sep = ';', index = False)
+
+data4 = pd.DataFrame()
+data4['WORDS'] = list(model.vocab.keys())[1500000:2000000]
+data4.to_csv('Data/Model_Vocabulary_part4.csv', sep = ';', index = False)
+
+data5 = pd.DataFrame()
+data5['WORDS'] = list(model.vocab.keys())[2500000:]
+data5.to_csv('Data/Model_Vocabulary_part5.csv', sep = ';', index = False)
