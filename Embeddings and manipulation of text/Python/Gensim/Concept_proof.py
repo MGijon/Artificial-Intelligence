f7600ab18b8e5876269b255678c8d572887ebb29
Unknown <mgijon94@gmail.com>
2018-04-20 00:05:05 +0200

    updated

---
+++ Embeddings and manipulation of text/Python/Gensim/Concept_proof.py
@@ -0,0 +1,30 @@
+import gensim.models as gm
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# ruta mac:
+route = '/Users/manuelgijonagudo/Documents/Programación/GIT/Data/GoogleNews-vectors-negative300.bin.gz'
+# ruta windows:
+#route = 'D:\GIT\Data\GoogleNews-vectors-negative300.bin.gz'
+model = gm.KeyedVectors.load_word2vec_format(route, binary = True)
+
+words = list(model.vocab.keys())[:1000]
+
+data = pd.DataFrame()
+data['words'] = words
+data['values'] = [model.most_similar( positive = ['women', i],
+                  negative = ['men'], topn = 1)[0][1] for i in data['words']]
+
+data.to_csv('Data/Concept_proof.csv', index = False)
+
+####################################
+
+datos = pd.read_csv('Data/Concept_proof.csv')
+valores = datos['values']
+len(valores)
+
+# hacemos una primera visualización de los datos mediante un simple histograma
+plt.figure(figsize = (10, 7.5))
+valores.plot(kind = 'hist', bins = 50)
+plt.show()
