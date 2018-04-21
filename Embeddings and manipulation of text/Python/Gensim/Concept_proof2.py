2e7af1f953fdd865a029119f40d1b385e3889ecb
Unknown <mgijon94@gmail.com>
2018-04-21 22:36:07 +0200

    updated

--- Embeddings and manipulation of text/Python/Gensim/Concept_proof2.py
+++ Embeddings and manipulation of text/Python/Gensim/Concept_proof2.py
@@ -11,9 +11,12 @@ words = list(model.vocab.keys())
 data = pd.read_csv('Data/Concept_proof2.csv')
 data['values'] = [0 for i in range(0, len(data['words']))]
 
-for i in range(0, 1000): # rango en el que calculamos
-    data['values']
-data['values'] = [model.most_similar( positive = ['women', i],
-                  negative = ['men'], topn = 1)[0][1] for i in data['words']]
-
+# rango en el que calculamos:
+for i in range(0, 1000):
+    try:
+        data['values'][i] = model.most_similar( positive = ['women', data['words'][i]],
+                      negative = ['men'], topn = 1)[0][1]
+    except UnicodeEncodeError:
+        pass
+# pasamos los datos al fichero:
 data.to_csv('Data/Concept_proof2.csv', index = False)
