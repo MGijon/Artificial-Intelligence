cc8a78e2feff51969b5dc2fc457910ab37417133
Unknown <mgijon94@gmail.com>
2018-05-02 18:33:40 +0200

    updated

--- Self-work Python/Data Exploration/Concejales_Madrid.py
+++ Self-work Python/Data Exploration/Concejales_Madrid.py
@@ -2,4 +2,14 @@ import pandas as pd
 
 data = pd.read_csv('Data/concejales_historico.csv', sep = ';')
 
-data.head()
+data.describe()
+
+#data['Duración'] = data['Fecha de cese'] - data['Fecha de posesión']
+
+# mirar como manejar fechas en python !!!!
+
+for i in set(data['Grupo municipal/Lista electoral']):
+    print(i)
+
+data['Nombre completo'] = data['Nombre'] + data['Apellidos']
+print(len(set(data['Nombre completo'])))
