95df34500afae22af3383b82d57731792594ed7f
Unknown <mgijon94@gmail.com>
2018-05-10 20:43:04 +0200

    updated

--- Embeddings and manipulation of text/Python/Gensim/Concept_proof4.py
+++ Embeddings and manipulation of text/Python/Gensim/Concept_proof4.py
@@ -7,15 +7,13 @@ route = '/Users/manuelgijonagudo/Documents/Programación/GIT/Data/GoogleNews-vec
 #route = 'D:\GIT\Data\GoogleNews-vectors-negative300.bin.gz'
 model = gm.KeyedVectors.load_word2vec_format(route, binary = True)
 
-palabra = 'king'
-resultado = model.most_similar(positive = ['women', palabra], negative = ['men'])
-
-resultado[0][0]
-
 def back_fort(word):
+    '''
+    Devuelve una tupla: palabra, distancia
+    '''
     resultado = model.most_similar(positive = ['women', word], negative = ['men'])
     resultado = model.most_similar(positive = ['men', resultado[0][0]], negative = ['women'])
 
     return resultado[0]
 
-back_fort('king')
+vocabulario = model.
