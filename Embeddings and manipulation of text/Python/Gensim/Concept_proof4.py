9371b253dc9e3415bdcdf5f3d208ef85ee1288d8
Unknown <mgijon94@gmail.com>
2018-05-13 22:12:53 +0200

    updated

--- Embeddings and manipulation of text/Python/Gensim/Concept_proof4.py
+++ Embeddings and manipulation of text/Python/Gensim/Concept_proof4.py
@@ -9,17 +9,56 @@ def back_fort(word):
     '''
     Devuelve una tupla: palabra, distancia
     '''
-    resultado1 = model.most_similar(positive = ['women', word], negative = ['men'])
-    resultado2 = model.most_similar(positive = ['men', resultado1[0][0]], negative = ['women'])
+    back = model.most_similar(positive = ['women', word], negative = ['men'])
+    fort = model.most_similar(positive = ['men', back[0][0]], negative = ['women'])
     #count = model.vocab[word][0]
     #index = model.vocab[word][1]
     word_vector = model.word_vec(word)
+    back_vector = model.word_vec(back[0][0])
+    fort_vector = model.word_vec(fort[0][0])
 
-    return (word, word_vector, resultado1[0][0], resultado1[0][1],
-            resultado2[0][0], resultado2[0][1])
+    return (word, word_vector, back[0][0], back_vector, back[0][1],
+            fort[0][0], fort_vector, fort[0][1])
 
-vocabulario = list(model.vocab)
+test_list = ['king', 'computer', 'work', 'kind', 'house', 'dog']
 
-len(vocabulario)
+df = pd.DataFrame()
+df['Word'] = []
+df['Word_vector'] = []
+df['Back_resoult'] = []
+df['Back_vector'] = []
+df['Back_distance'] = []
+df['Fort_resoult'] = []
+df['Fort_vector'] = []
+df['Fort_distance'] = []
 
-back_fort('home')
+Word = []
+Word_vector= []
+Back_resoult = []
+Back_vector = []
+Back_distance = []
+Fort_resoult = []
+Fort_vector = []
+Fort_distance = []
+
+for w in test_list:
+    aux = back_fort(w)
+    Word.append(aux[0])
+    Word_vector.append(aux[1])
+    Back_resoult.append(aux[2])
+    Back_vector.append(aux[3])
+    Back_distance.append(aux[4])
+    Fort_resoult.append(aux[5])
+    Fort_vector.append(aux[6])
+    Fort_distance.append(aux[7])
+
+df['Word'] = Word
+df['Word_vector'] = Word_vector
+df['Back_resoult'] = Back_resoult
+df['Back_vector'] = Back_vector
+df['Back_distance'] = Back_distance
+df['Fort_resoult'] = Fort_resoult
+df['Fort_vector'] = Fort_vector
+df['Fort_distance'] = Fort_distance
+
+df.to_csv('Data/Concept_proof4_1')
