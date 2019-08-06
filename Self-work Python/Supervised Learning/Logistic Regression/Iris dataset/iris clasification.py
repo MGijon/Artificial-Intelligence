389b1a02e8957afc0681fb7d01a2700e48bfba80
Unknown <mgijon94@gmail.com>
2019-08-06 18:51:38 +0200

    new example of logistic regression

--- Self-work Python/Supervised Learning/Logistic Regression/Iris dataset/iris clasification.py
+++ Self-work Python/Supervised Learning/Logistic Regression/Iris dataset/iris clasification.py
@@ -9,7 +9,7 @@ import os
 
 # import some data to play with
 iris = datasets.load_iris()
-X = iris.data[:, :2]  # we only take the first two features.
+X = iris.data[:, :2]  # we only take the first two features
 Y = iris.target
 
 logreg = linear_model.LogisticRegression(C=1e5, solver='lbfgs', multi_class='multinomial') 
@@ -42,9 +42,45 @@ plt.xlim(xx.min(), xx.max())
 plt.ylim(yy.min(), yy.max())
 plt.xticks(())
 plt.yticks(())
-plt.savefig("Images/iris_Sepal_LR.png")
+#plt.savefig("Images/iris_Sepal_LR.png")
 plt.show()
 
+##############################################################
+X = iris.data[:, 2:]  # Petal
+Y = iris.target
 
+
+logreg = linear_model.LogisticRegression(C=1e5, solver='lbfgs', multi_class='multinomial') 
+
+# Create an instance of Logistic Regression Classifier and fit the data.
+logreg.fit(X, Y)
+
+# Plot the decision boundary. For that, we will assign a color to each
+# point in the mesh [x_min, x_max]x[y_min, y_max].
+x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+h = .02  # step size in the mesh
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z = logreg.predict(np.c_[xx.ravel(), yy.ravel()])
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+plt.figure(1, figsize=(4, 3))
+plt.pcolormesh(xx, yy, Z, cmap=plt.cm.Paired)
+
+
+## Plot Sepal
+## ==========
+
+#plt.figure(figsize=(20, 14))
+plt.scatter(X[:, 0], X[:, 1], c=Y, edgecolors='k', cmap=plt.cm.Paired)
+plt.xlabel('Petal length')
+plt.ylabel('Petal width')
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+plt.xticks(())
+plt.yticks(())
+plt.savefig("Images/iris_Petal_LR.png")
+plt.show()
 # https://seaborn.pydata.org/generated/seaborn.scatterplot.html
 # https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
